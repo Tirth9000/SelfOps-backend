49883423ebc91f81291dbe69f9f67de2921dbbe5
Tirth9000 <sharmatirth09012@gmail.com>
2025-08-26 12:21:47 +0530

    Refactor project structure: update .gitignore, enhance README, fix import paths, and improve authentication logic

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from .routes import cli_main, web_main
+from routes import cli_main, web_main
 
 
 app = FastAPI(title = "SELFOPS BDE")
 
 app.include_router(cli_main.router, prefix="/cli", tags=["CLI"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", port=8000, reload=True)
