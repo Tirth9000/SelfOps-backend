e6f18b907778c1ddfbc64bd63c9465c065b07d59
Tirth9000 <sharmatirth09012@gmail.com>
2025-09-07 02:15:07 +0530

    Enhance authentication flow: update .gitignore, refactor user schema, implement JWT token handling, and add login required middleware

--- db/schema.py
+++ db/schema.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    username: str = Field(..., min_length=6, max_length=20, description="Username must be between 6 and 20 characters.")
-    password: str = Field(..., min_length=8, max_length=50, description="Password must be between 8 and 50 characters.")
-    # username: str
-    # password: str
+    # username: str = Field(..., min_length=6, max_length=20, description="Username must be between 6 and 20 characters.")
+    # password: str = Field(..., min_length=8, max_length=50, description="Password must be between 8 and 50 characters.")
+    username: str
+    password: str
