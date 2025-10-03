bc2aef614427313ec5119bd27cb223af74ee504b
Tirth9000 <sharmatirth09012@gmail.com>
2025-10-04 00:57:41 +0530

    Refactor: Remove unused components and files
    - Deleted HomeDashboard, Login, Navbar, ProtectedRoute, Signup components.
    - Removed associated CSS and configuration files.
    - Cleaned up test data and WebSocket test HTML files.
    - Streamlined project structure by eliminating unnecessary files.

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from db.models import User
 from routes.CLI.cli_main import router as cli_router
 from routes.WEB.web_main import router as web_router
 from fastapi.middleware.cors import CORSMiddleware
+from socket_server import socket_app
 
 app = FastAPI()
 
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow your frontend origin
+    allow_origins=["*"],  # Allow your frontend origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
@@ -22,19 +23,19 @@ async def init_db():
     await init_beanie(database=client.SelfOpsDB, document_models=[User])
 
 # Startup event to initialize database
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
+# @app.on_event("startup")
+# async def on_startup():
+#     await init_db()
 
 socket_app.other_asgi_app = app
 
 # Include routers
 app.include_router(cli_router, prefix="/cli", tags=["cli"])
-app.include_router(web_router, tags=["web"])
+app.include_router(web_router, prefix="/web", tags=["web"])
 
 
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(socket_app, host="localhost", port=8000)
