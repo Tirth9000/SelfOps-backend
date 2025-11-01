e9f9968fc7c65098bb01835164254fc762b5e40d
Tirth9000 <sharmatirth09012@gmail.com>
2025-11-01 11:47:27 +0530

    hello

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from backend.routes.CLI.auth_api import router as cli_router
-from backend.routes.WEB.web_api import router as web_router
+from routes.CLI.auth_api import router as cli_router
+from routes.WEB.web_api import router as web_router
 from fastapi.middleware.cors import CORSMiddleware
-from backend.socket_server import socket_app
-from backend.db.database import init_db
+from socket_server import socket_app
+from db.database import init_db
 import os
 
 
@@ -39,4 +39,4 @@ app.include_router(web_router, prefix="/web", tags=["web"])
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("backend.main:socket_app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:socket_app", host="0.0.0.0", port=port, reload=False)
