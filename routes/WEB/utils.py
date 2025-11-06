f0f762f8535cd95050d16c762a43219b6adedfd7
Tirth9000 <sharmatirth09012@gmail.com>
2025-11-06 11:54:42 +0530

    Fix Redis client initialization to enable SSL support

--- routes/WEB/utils.py
+++ routes/WEB/utils.py
@@ -16,7 +16,7 @@ oauth2_scheme = HTTPBearer()
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 # r_client = aioredis.from_url(config('REDIS_URL'), decode_responses=True)
-r_client = redis.Redis.from_url(config("REDIS_URL"), ssl=True)
+r_client = redis.Redis.from_url(config("REDIS_URL"), decode_responses=True)
 
 
 def hash_password(password: str) -> str:
